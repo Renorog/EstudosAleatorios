dbbcbd520f4066b894ba422fe258965b519e10a8
Renor Oliveira Guimarães <renornew@gmail.com>
2022-11-16 10:08:16 -0300

    Estudo Operador Ternário C#

---
+++ OperadorTernario/OperadorTernario/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperadorTernario {
+    internal class Program {
+        static void Main(string[] args) {
+            int numero = 10;
+            string mensagem = "";
+
+            //estrutura do operador ternário
+            // condição ? true : false;
+
+            mensagem = numero > 5 ? "Maior que 5" : "Menor que 5";
+            // a mensagem maior ou menor que 5 vai ser atribuida a variável mensagem depois de verificada se é verdadeira ou falsa
+
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperadorTernario/README.md
@@ -0,0 +1,3 @@
+# EstudosAleatorios
+Repositório reservado para estudos diversos.
+Este repositório está reservado para estudos aleatórios de diversas linguagens e todo tipo de exercicios e desafios relacionados a programação.
